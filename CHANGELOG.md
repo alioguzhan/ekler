fea6942ef6a8fe075df0de21b800f4c259f0f945
alioguzhan <aoguzhanyildiz@gmail.com>
2021-06-07 02:13:31 +0300

    chore: release v.0.6.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,25 +1,35 @@
 # CHANGELOG
 
+## 0.6.1
+
+- Docstringlerde guncel olmayan yerler duzletildi.
+
 ## 0.6.0
 
 - Vasita ekleri geldi. ([@midorikocak](https://github.com/midorikocak)'a tesekkurler.)
 
 ## 0.5.1
+
 - f string kullanimini kaldir. eski usul devam.
 
 ## 0.5
-- Cogul ekleri geldi. 
+
+- Cogul ekleri geldi.
 
 ## 0.4
+
 - Bulunma ve Ayrilma ekleri geldi.
 
 ## 0.3
+
 - Turkce olmayan kelimelerin sonuna gelen ek yanlisti. Duzeltildi.
 
 ## 0.2
+
 - Art arda 3 tane sessiz harf iceren kelimelere `anlamsiz` olarak islem yapiliyor
-- Buyuk harf ve mixedCase destegi 
+- Buyuk harf ve mixedCase destegi
 - Daha fazla test
 
 ## 0.1
+
 - Inital Release
--- setup.py
+++ setup.py
@@ -2,28 +2,35 @@ from setuptools import setup, find_packages
 
 
 classifiers = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3',
-    'Topic :: Utilities'
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "Topic :: Utilities",
 ]
 
 setup(
-    name='ekler',
-    version='0.6.0',
-    packages=find_packages(include=['ekler'],
-                           exclude=['tests*', '.vscode', 'venv', '__*', ]),
+    name="ekler",
+    version="0.6.1",
+    packages=find_packages(
+        include=["ekler"],
+        exclude=[
+            "tests*",
+            ".vscode",
+            "venv",
+            "__*",
+        ],
+    ),
     classifiers=classifiers,
     py_modules=["ekler"],
-    python_requires='>=3',
+    python_requires=">=3",
     keywords="turkish, turkce, isim, ekler",
-    license='MIT',
-    description='Turkce kelimelerin sonuna fonetik yapilarina uygun ekleri ekleyen module.',
-    long_description_content_type='text/markdown',
-    long_description=open('README.md').read(),
-    url='https://github.com/alioguzhan/ekler',
-    author='Ali Oguzhan Yildiz',
-    author_email='aoguzhanyildiz@gmail.com'
+    license="MIT",
+    description="Turkce kelimelerin sonuna fonetik yapilarina uygun ekleri ekleyen module.",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    url="https://github.com/alioguzhan/ekler",
+    author="Ali Oguzhan Yildiz",
+    author_email="aoguzhanyildiz@gmail.com",
 )
